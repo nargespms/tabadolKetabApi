45b25e5f37c9e0f0d9f1cda1524681552f599221
narges <nargespms.np@gmail.com>
2020-10-18 15:31:16 +0330

    add ticet list

---
+++ schemas/tickets/ticketsLIst.js
@@ -0,0 +1,65 @@
+
+
+const ticketsList = {
+  $id: 'tickets list',
+  title: 'tickets List',
+  description: 'List for tickets',
+  type: 'array',
+  properties: {
+    result: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+            id: {
+              type: 'string',
+            },
+            title: {
+              type: 'string',
+            },
+            summaryText: {
+              type: 'string',
+            },
+            sendDate: {
+              type: 'string',
+            },
+            receiver:{
+                type:Object,
+                properties: {
+                    id: {
+                      type: 'string',
+                    },
+                   firstName: {
+                      type: 'string',
+                    },
+                    lastName: {
+                      type: 'string',
+                    },
+                }
+            },
+            sender:{
+                type:Object,
+                properties: {
+                    id: {
+                      type: 'string',
+                    },
+                   firstName: {
+                      type: 'string',
+                    },
+                    lastName: {
+                      type: 'string',
+                    },
+                    mobile: {
+                      type: 'string',
+                    },
+                }
+            },
+        },
+      },
+    },
+  },
+};
+
+module.exports = {
+  ticketsList,
+};
