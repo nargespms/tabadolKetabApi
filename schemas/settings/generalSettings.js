54d8e15b71c4d3b1264742db89ea2a8e959c5ddd
narges <nargespms.np@gmail.com>
2020-11-24 12:42:59 +0330

    add settings

---
+++ schemas/settings/generalSettings.js
@@ -0,0 +1,44 @@
+
+const generalSettings = {
+  $id: 'generalSettings',
+  title: 'generalSettings ',
+  description: 'generalSettings',
+  type: 'object',
+  properties: {
+    generalSettings: {
+      type: 'object',
+      properties: {
+        numberOfFreePostOfBook:{
+            type:'string',
+        },
+        postPrice:{
+            type:'number'
+        }
+        creditExpireTimeFromSellBook:{
+            type:'string'
+        }
+        tabadolWageForSell:{
+            type:'number'
+        }
+        discountForRangeDate:{
+            type:'object'
+            properties:{
+                from:{
+                    type:'number'
+                },
+                to:{
+                    type:'number'
+                },
+                percent:{
+                    type:'number'
+                },
+            }
+        }
+      },
+    },
+  },
+};
+
+module.exports = {
+  generalSettings,
+};
